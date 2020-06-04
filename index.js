b4bde058c7d9cf0c2fd97e80c33381397c2e8b42
Stephan Bakkelund Valois <stephan@devalo.no>
2020-06-04 20:31:29 +0200

    phonebook backend step 3

--- index.js
+++ index.js
@@ -38,6 +38,17 @@ app.get('/api/persons', (req, res) => {
 
 });
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
 
 
 
