2586476fe77a4c5e8b8ceb627add26c51980b073
Stephan Bakkelund Valois <stephan@devalo.no>
2020-06-06 09:13:54 +0200

    uibuild

---
+++ build/precache-manifest.dfaa8db18205de4fa95a62a1f40cc934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c3e4945feaf4e04ed9f273c96e3cda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc85340a20fdb83cc38d",
+    "url": "/static/css/main.be29676f.chunk.css"
+  },
+  {
+    "revision": "e690b7896d4bc35c0816",
+    "url": "/static/js/2.ddec9da4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ddec9da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc85340a20fdb83cc38d",
+    "url": "/static/js/main.05710ef2.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
 app.use(morgan(':method test :status :response-time ms - :res[content-length] :body'))
 
 
